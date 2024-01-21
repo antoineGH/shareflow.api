0d233495c0d361798561f5c49f3c1d277ddb8b8b
antoineGH <antoine.ratat@gmail.com>
2024-01-22 04:20:29 +1100

    add port to db conf

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 const pool = new Pool({
   host: process.env.PG_HOST,
   user: process.env.PG_USER,
+  port: process.env.PG_PORT,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
 });
